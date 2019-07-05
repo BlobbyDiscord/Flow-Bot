234cce1af3fe99ff0095a7a991e22ce0ee37a990
BlobbyDiscord <44066266+BlobbyDiscord@users.noreply.github.com>
2019-07-05 13:06:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
  
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
+    if (message.content === '//rules') {
+       message.reply('Check out #rules');
        }
 });
  
