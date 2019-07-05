f198bc48db79549179c8589623add472c3e29528
BlobbyDiscord <44066266+BlobbyDiscord@users.noreply.github.com>
2019-07-05 21:01:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,31 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
  
+
+client.on('message', message => {
+    if (message.content === 'hi') {
+       message.reply('Hello');
+       }
+});
+
+client.on('message', message => {
+    if (message.content === 'wat') {
+       message.reply('??');
+       }
+});
+
+client.on('message', message => {
+    if (message.content === 'lol') {
+       message.reply('Whats funny?');
+       }
+});
+
+client.on('message', message => {
+    if (message.content === 'ANNOUNCE25091') {
+       message.reply('Discord Server Opened');
+       }
+});
+
 client.on('message', message => {
     if (message.content === '//rules') {
        message.reply('Check out #rules');
@@ -25,8 +50,21 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '//links') {
+       message.reply('Run ```//links1``` for Radio link! Run ```//links2``` for Rules! Run ```//links3// for COmmands!');
+       }
+});
+client.on('message', message => {
+    if (message.content === '//links1') {
        message.reply(':link: https://flowradio.ml/ **Radio**');
+       }
+});
+client.on('message', message => {
+    if (message.content === '//links2') {
        message.reply(':link: https://flowradio.ml/rules **Rules**');
+       }
+});
+client.on('message', message => {
+    if (message.content === '//links3') {
        message.reply(':link: https://flowradio.ml/commands **Commands**');
        }
 });
